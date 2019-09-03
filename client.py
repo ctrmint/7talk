e356f69066c4c33629b57c3d875ab0e2a87d763d
mark <mvrodman@msn.com>
2019-09-03 17:19:28 +0100

    working on can bus socket messaging structure and control

--- client.py
+++ client.py
@@ -14,7 +14,6 @@ import binascii, socket, struct, sys, random
 from dash_support import *
 
 
-
 def display_data_structure():
     print("The following structure is used;")
     for i in range(len(data_value_labels)):
@@ -37,37 +36,46 @@ def gen_data(b, d):
     return values
 
 
-def main():
-    host = 'localhost'
-    udp_port = 11111
-    fmt = struct.Struct('I I 20s I')
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', udp_port)
+def packing(fmt, unpacked_data):
+    packed_data = fmt.pack(*unpacked_data)
+    return packed_data
 
+
+def send_data(packed_data):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # creating socket with these two lines
+    server_address = (server_addr, server_udp_port)  # need to be in loop otherwise errors!
+
+    try:  # try ...
+        sock.connect(server_address)  # and connect to the dash server socket
+        try:  # connected...
+            sock.send(packed_data)  # send the packet.
+        finally:
+            sock.close()  # now close the socket!
+            success = True
+
+    except socket.error as msg:  # initial try to connect failed!!
+        print("Couldn't connect with the server: %s." % msg)  # error and try again with while loop!
+        # will print to screen failure
+        success = False
+    return success
+
+def main():
+    display_data_structure()                                                # Print som details regarding structure
+    fmt = struct.Struct('I I 20s I')                                        # format of packing structure
     b = 0
     d = 0
-    display_data_structure()
-
     while 1:
         b += 1
         if b > 10:
             b = 0
-        values = gen_data(b, d)
-        packed_data = fmt.pack(*values)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)            # creating socket with these two lines
-        server_address = (server_addr, server_udp_port)                     # need to be in loop otherwise errors!
 
-        try:                                                                # try ...
-            sock.connect(server_address)                                    #    and connect to the dash server socket
-            try:                                                            # connected...
-                sock.send(packed_data)                                      #    send the packet.
-            finally:
-                sock.close()                                                # now close the socket!
+        values = gen_data(b, d)                                     # generate some test values, needs a better function
 
-        except socket.error as msg:                                         # initial try to connect failed!!
-            print("Couldn't connect with the server: %s." % msg)            # error and try again with while loop!
-                                                                            # will print to screen failure
+        packed_data = packing(fmt, values)                          # pack the data to be sent via socket
+        send_data(packed_data)                                      # send packed data via function.
+
+
+
 
 if __name__ == '__main__':
     main()
